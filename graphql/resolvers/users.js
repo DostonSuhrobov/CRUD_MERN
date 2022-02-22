2bb793446019f336f9a42dc1485f4e07b6f25fb4
Doston <Doston>
2022-02-21 20:46:11 -0500

    some validation stuff

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,63 @@
 const User = require('../../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { UserInputError } = require('apollo-server');
+
+const {validateRegisterInput} = require('../../util/validators');
+const { SECRET_KEY } = require('../../config');
 
 module.exports = { 
     Mutation: {
-        register(_, args, context, info){
-            // TODO validate data
-            // TODO make sure user does not already exist
-            // TODO create authentication token
+       async register(_, { registerInput : { 
+            username, 
+            email,
+            password, 
+            confirmPassword 
+        } },
+         context,
+         info
+        ){
+            //  validate data
+            const { errors, valid  } = validateRegisterInput(username, email, password, confirmPassword);
+            if(!valid)
+            {
+                throw new UserInputError('Error', { errors });
+            }
+
+            //  make sure user does not already exist
+            const user = await User.findOne({ username });
+            if(user){
+                throw new UserInputError('Username is taken', {
+                    errors: {
+                        username: 'This username is taken'
+                    }
+                })
+            }
+
+            //  authentication token
+            password = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            });
+
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, { expiresIn: '1h'});
+
+            return {
+                ...res._doc,
+                id:res._id,
+                token
+            }
         }
     }
 }
